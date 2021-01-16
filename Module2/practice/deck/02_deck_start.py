83fde0a2bd652977558ab56c2f78f202d13fde7c
Paul <37895059+psvtg@users.noreply.github.com>
2021-01-16 17:13:43 +0300

    Update 02_deck_start.py

--- Module2/practice/deck/02_deck_start.py
+++ Module2/practice/deck/02_deck_start.py
@@ -1,9 +1,20 @@
+import random
+
+
+values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+types = {'hearts': '\u2665',
+         'diamonds': '\u2666',
+         'clubs': '\u2663',
+         'spades': '\u2660'}
+
+
 class Card:
     def __init__(self, value, type):
-        pass
+        self.value = value
+        self.type = type
 
     def to_str(self):
-        pass
+        return f"{self.value} {types[self.type]}"
 
 
 class Deck:
@@ -11,16 +22,37 @@ class Deck:
         self.cards = []
 
     def show(self):
-        pass
+        for card in self.cards:
+            print(card.to_str())
 
     def draw(self, x):
-        pass
+        drawed = []
+        for i in range(x):
+            out = self.cards[i].pop()
+            drawed.append(out.to_str())
+        return drawed
 
     def shuffle(self):
-        pass
+        return random.shuffle(self.cards)
 
 
-deck = Deck()
-print(deck.show())
-deck.shuffle()
-print(deck.show())
+def main():
+    mydeck = []
+    for type in types:
+        for value in values:
+            card = Card(value, type)
+            mydeck.append(card)
+
+    deck = Deck()
+    deck.cards = mydeck
+
+    print(deck.show())
+    deck.shuffle()
+    print(deck.show())
+    print('DRAW')
+    print(deck.draw(5))
+
+
+
+if __name__ == '__main__':
+    main()
